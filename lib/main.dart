b961eb5e6151537cea4a16cf8e9e5513a663f32d
Alessa-Gillespie <103238556+Alessa-Gillespie@users.noreply.github.com>
2023-11-27 20:59:13 +0800

    Testing

--- lib/main.dart
+++ lib/main.dart
@@ -53,7 +53,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     // theme: theme,
+     // theme: theme, //Testing
       title: 'kusina_v3',
       debugShowCheckedModeBanner: false, // (^3)
       initialRoute: AppRoutes.ingredientsPageScreen,
